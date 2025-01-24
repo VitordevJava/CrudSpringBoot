57ade396cb13ad847556edc13f153d9b2b17dbda
VitordevJava <vitordev.java@gmail.com>
2025-01-23 23:09:56 -0300

    Correção de erro, ao procurar um index html, ele acabava entrando na primeira requisição do RequestMapping trazendo a frente apenas O nome Spring Boot Rest API CRUD,erro corrigido adicionando o greet a esse Request Maping

--- src/main/java/CrudSpringRest/RestApiCrud/controllers/GreetingsController.java
+++ src/main/java/CrudSpringRest/RestApiCrud/controllers/GreetingsController.java
@@ -36,7 +36,7 @@ public class GreetingsController {
 	 * @param name the name to greet
 	 * @return greeting text
 	 */
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	@RequestMapping(value = "/greet/{name}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public String greetingText(@PathVariable String name) {
 		return "Spring Boot Rest API CRUD " + name + "!";
